7d8ccffe296ae50959ed27993335819f54e15981
William <codinginthestars@gmail.com>
2024-10-14 16:22:41 -0500

    batman

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import './Navbar.css';
+import cart_outline from '../assets/cart-outline.png';
+import cart from '../assets/cart.png';
+import king from '../assets/chess-king.png';
+import queen from '../assets/chess-queen.png';
+import sun from '../assets/weather-sunny.png';
+import moon from '../assets/moon-waxing-crescent.png';
+import { useState } from 'react';
+
+function Navbar() {
+
+    const [theme, setTheme] = useState("light");
+
+
+    return (
+        <div className="navbar">
+            <div className='navbar-left'>
+                <img src={king} />
+                <h2>RoyalTy Demands</h2>
+                <img src={queen} alt='' />
+            </div>
+            <div className='navbar-middle'>
+                <ul>
+                    <li>Home</li>
+                    <li>Shop</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div className='navbar-right'>
+                { theme === "light" ? <img src={sun} alt='' onClick={() => {setTheme("dark")}} /> : <img src={moon} alt='' onClick={() => {setTheme("light")}} />}
+                { theme === "light" ? <img src={cart} alt='' /> : <img src={cart_outline} alt='' />}
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+function Home() {
+
+    return (
+        <div className="home-page">
+            <Navbar/>
+        </div>
+    )
+};
+
+export default Home;
